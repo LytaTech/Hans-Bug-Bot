0b04ddb09869c3f21aa8d42db47e3ec9a16c8b3f
LytaTech <johnnyfavour131@gmail.com>
2025-03-13 15:10:22 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,45 +8,45 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "237696900612"
-global.ownername = "*Hans Tech"
-global.ytname = "YT: HansTech0"
-global.socialm = ""
+global.ownernomer = "2347084528285"
+global.ownername = "*lyta Tech"
+global.ytname = "YT: little lyta"
+global.socialm = "little LYTA"
 global.location = "EARTH,AFRICA"
 
 global.ownernumber = '237696900612'  //creator number
-global.ownername = 'Harold MTH' //owner name
-global.botname = 'Hans-Bug-bot' //name of the bot
+global.ownername = 'lyta MTH' //owner name
+global.botname = 'little LYTA' //name of the bot
 
 //sticker details
-global.packname = 'Sticker By Hans Tech'
-global.author = 'Hallo\n\nContact: 237696900612'
+global.packname = 'Sticker By little LYTA'
+global.author = 'Hallo\n\nContact: 2347084528285'
 
 //console view/theme
 global.themeemoji = '😎'
-global.wm = "Hans Tech😏"
+global.wm = "LYTA Tech😏"
 
 //theme link
-global.link = 'https://whatsapp.com/channel/0029VaZDIdxDTkKB4JSWUk1O'
+global.link = 'https://m.webnovel.com/book/reincarnated-as-a-mushroom_31834764608814605'
 
 //custom prefix
 global.prefa = ['','!','.','#','&']
 
 //false=disable and true=enable
 global.autoRecording = false //auto recording
-global.autoTyping = true //auto typing
+global.autoTyping = false //auto typing
 global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
-global.autoswview = false //auto view status/story
+global.autoswview = true //auto view status/story
 
 //menu type 
 //v1 is image menu, 
 //v2 is link + image menu,
 //v3 is video menu,
 //v4 is call end menu
-global.typemenu = 'v1'
+global.typemenu = 'v3'
 
 //reply messages
 global.mess = {
